7b25dc242c7ce4628bf227207f9b0889ca04281b
Diego57709 <chuhanli2005@gmail.com>
2025-01-24 13:46:25 +0100

    gestionar terminado

--- gestionarServicio.sh
+++ gestionarServicio.sh
@@ -7,12 +7,36 @@ function gestionarServicio() {
     read -p "Seleccione una acción para dnsmasq: " opcionGestion
     if [ $opcionGestion -eq 1 ]; then
         sudo systemctl start dnsmasq
+        resultado=$?
+        if [ $? ]; then
+            echo "Servicio iniciado con exito"
+        else
+            echo "Error al iniciado el servicio, intentalo de nuevo"
+        fi
     elif [ $opcionGestion -eq 2 ]; then
         sudo systemctl stop dnsmasq
+        resultado=$?
+        if [ $? ]; then
+            echo "Servicio detenido con exito"
+        else
+            echo "Error al parar el servicio, intentalo de nuevo"
+        fi
     elif [ $opcionGestion -eq 3 ]; then
         sudo systemctl restart dnsmasq
+        resultado=$?
+        if [ $? ]; then
+            echo "Servicio reiniciado con exito"
+        else
+            echo "Error al reiniciar el servicio, intentalo de nuevo"
+        fi
     elif [ $opcionGestion -eq 4 ]; then
-        sudo systemctl status dnsmasq
+        sudo systemctl --no-pager status dnsmasq
+        resultado=$?
+        if [ $? ]; then
+            echo "Datos del servicio: "
+        else
+            echo "Error al conseguir el estado del servicio, intentalo de nuevo"
+        fi
     fi
 }
 gestionarServicio
